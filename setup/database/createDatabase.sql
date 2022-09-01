712ac78be594c2bd12c366ea51c11aa8ed8156c6
Jere Stigell <jj.stigell@gmail.com>
2022-09-01 16:42:41 +0900

    example words table, drops tables file

--- setup/database/createDatabase.sql
+++ setup/database/createDatabase.sql
@@ -1,12 +1,30 @@
-CREATE EXTENSION CITEXT;
+CREATE EXTENSION IF NOT EXISTS CITEXT;
 
 -- DOMAIN email makes constraint check that the users email is valid
-CREATE DOMAIN valid_email AS CITEXT CHECK ( VALUE ~ '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$' );
+CREATE DOMAIN IF NOT EXISTS valid_email AS CITEXT CHECK ( VALUE ~ '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$' );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   email valid_email NOT NULL UNIQUE,
   password CHAR(60) NOT NULL,
-  username VARCHAR(15) NOT NULL UNIQUE,
+  username CITEXT NOT NULL UNIQUE,
   user_created DATE NOT NULL DEFAULT NOW()
+);
+
+-- Example words for the kanji in question
+CREATE TABLE IF NOT EXISTS example_words (
+  id SERIAL PRIMARY KEY,
+  word TEXT UNIQUE,
+  furigana TEXT,
+  jlpt_level INTEGER
+);
+
+-- Translation for the word, multiple languages (e.g. 'fi', 'en'), country IDs in ISO 639-1
+CREATE TABLE IF NOT EXISTS example_word_translations (
+  id SERIAL PRIMARY KEY,
+  word_id INTEGER,
+  language CHAR(2),
+  translation TEXT,
+  description TEXT,
+  FOREIGN KEY (word_id) REFERENCES example_words(id)
 );
---
+++ setup/database/dropTables.sql
@@ -0,0 +1,5 @@
+DROP DOMAIN valid_email;
+DROP TABLE users;
+
+-- drops also word translations
+DROP TABLE example_words CASCADE;
