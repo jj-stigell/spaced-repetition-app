cd51607de98a9b8d121d45057d460e0c8ddcf784
Jere Stigell <jj.stigell@gmail.com>
2022-09-07 00:51:21 +0900

    env imports from config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-const express = require('express');
+import express, { Request, Response } from "express";
+import POSTGRE_URL from "./util/config";
 const app = express();
 
-app.get('/', (_req: any, res: any) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('hello');
 });
 
-const PORT: number = 3003;
+const PORT = 3003;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(POSTGRE_URL);
 });
--- src/util/config.ts
+++ src/util/config.ts
@@ -1,5 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
+require('dotenv').config();
 const POSTGRE_URL = process.env.POSTGRE_URL;
 
-module.exports = {
-  POSTGRE_URL,
+export default {
+  POSTGRE_URL
 };
