d4066a6cec3ab43c6fe2f9f9e173767d1194a70f
Jere Stigell <jj.stigell@gmail.com>
2022-09-07 16:27:03 +0900

    logger exports

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,7 +1,12 @@
-export const info = (...params: Array<string|unknown>) => {
+const info = (...params: Array<string|unknown>) => {
   console.log(...params);
 };
 
-export const error = (...params: Array<string|unknown>) => {
+const error = (...params: Array<string|unknown>) => {
   console.error(...params);
+};
+
+export {
+  info,
+  error
 };
