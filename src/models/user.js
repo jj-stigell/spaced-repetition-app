1f9c1e57efb4a60dd018141c912ee3e58a4d355c
Jere Stigell <jj.stigell@gmail.com>
2022-09-09 15:44:43 +0900

    user model

---
+++ src/models/user.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const { sequelize } = require('../util/database');
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.CHAR(60),
+    allowNull: false
+  },
+  username: {
+    type: DataTypes.STRING(12),
+    unique: true,
+    allowNull: false,
+    validate: {
+      len: [4, 12]
+    }
+  },
+  member: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true
+  },
+  emailVerified: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  admin: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+}, {
+  sequelize,
+  underscored: true,
+  modelName: 'user'
+});
+
+module.exports = User;
