9c0b221d7aa40c8c94863585baef9736950554a3
Jere Stigell <jj.stigell@gmail.com>
2022-09-10 16:06:39 +0900

    rollback func and initialize db with account table

---
+++ migrations/20220909_00_initialize.js
@@ -0,0 +1,66 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('account', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      email: {
+        type: DataTypes.STRING(255),
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        }
+      },
+      password: {
+        type: DataTypes.CHAR(60),
+        allowNull: false
+      },
+      username: {
+        type: DataTypes.STRING(14),
+        unique: true,
+        allowNull: false,
+        validate: {
+          len: [1, 14],
+        }
+      },
+      member: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      },
+      emailVerified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      admin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      },
+      lastLogin: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      }
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('account');
+  },
+};
--- src/util/rollback.js
+++ src/util/rollback.js
@@ -1,3 +1,9 @@
 const { rollbackMigration } = require('./database');
+const { ENVIRONMENT, NODE_ENV } = require('./config');
 
-rollbackMigration();
+if (ENVIRONMENT.PRODUCTION) {
+  console.log('Be careful when rolling back, this is production version');
+} else {
+  console.log('Rolling back on:', NODE_ENV);
+  rollbackMigration();
+}
