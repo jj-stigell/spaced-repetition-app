4eeb6eeb7ecc970255e1198eea8064f3d26d51a3
Jere Stigell <jj.stigell@gmail.com>
2022-09-10 16:06:57 +0900

    db structure change

--- src/util/database.js
+++ src/util/database.js
@@ -7,21 +7,14 @@ const sequelize = new Sequelize(DATABASE_URL, {
     ssl: {
       require: true,
       rejectUnauthorized: false
-    }
+    },
+  },
+  define: {
+    timestamps: true,
+    freezeTableName: true,        // Freeze names, no plural names
+    underscored: true,
   },
 });
-
-const connectToDatabase = async () => {
-  try {
-    await sequelize.authenticate();
-    await runMigrations();
-    console.log('database connected');
-  } catch (err) {
-    console.log('connecting database failed', err);
-    return process.exit(1);
-  }
-  return null;
-};
 
 const migrationConf = {
   migrations: {
@@ -45,6 +38,18 @@ const rollbackMigration = async () => {
   const migrator = new Umzug(migrationConf);
   await migrator.down();
   sequelize.close();
+};
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    await runMigrations();
+    console.log('database connected');
+  } catch (err) {
+    console.log('connecting database failed', err);
+    return process.exit(1);
+  }
+  return null;
 };
 
 module.exports = {
