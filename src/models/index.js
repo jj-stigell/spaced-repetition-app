e45e913b7f38442179332f53a2890eaa136891e7
Jere Stigell <jj.stigell@gmail.com>
2022-09-13 15:47:30 +0900

    radical translation model

--- src/models/index.js
+++ src/models/index.js
@@ -3,10 +3,12 @@ const Account = require('./account');
 const Kanji = require('./kanji');
 const Country = require('./country');
 const Radical = require('./country');
+const RadicalTranslation = require('./radicalTranslation');
 
 module.exports = {
   Account,
   Kanji,
   Country,
-  Radical
+  Radical,
+  RadicalTranslation
 };
---
+++ src/models/radicalTranslation.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const { sequelize } = require('../util/database');
+
+class RadicalTranslation extends Model {}
+
+RadicalTranslation.init({
+  radicalId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'radical',
+      key: 'id'
+    }
+  },
+  languageId: {
+    type: DataTypes.CHAR(2),
+    allowNull: false,
+    references: {
+      model: 'country',
+      key: 'language_id'
+    }
+  },
+  translation: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+  },
+}, {
+  sequelize,
+  modelName: 'radicalTranslation'
+});
+
+module.exports = RadicalTranslation;
