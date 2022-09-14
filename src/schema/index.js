4fa9d120352996ca4926bbe5618c5d89b7dc0a42
Jere Stigell <jj.stigell@gmail.com>
2022-09-14 13:22:25 +0900

    refactor account resolvers and typedefs to folder

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,7 +1,7 @@
 /** Create an executable scheme from all the typedefs and merged resolvers */
 const { merge } = require('lodash');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
-const { typeDef: Account, resolvers: accountResolvers } = require('./account.js');
+const { typeDef: Account, resolvers: accountResolvers } = require('./account/');
 
 const schema = makeExecutableSchema({
   typeDefs: [ Account ],
