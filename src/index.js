9d46177bc02cad53ec099aa62acf1ba5003e86da
Jere Stigell <jj.stigell@gmail.com>
2022-09-14 13:13:04 +0900

    edit context to find account

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 
 const { ApolloServer } = require('apollo-server');
-const { JWT_SECRET } = require('./util/config');
+const { JWT_SECRET, ENVIRONMENT } = require('./util/config');
 const { connectToDatabase } = require('./util/database');
 const schema = require('./schema');
 const jwt = require('jsonwebtoken');
+const { Account } = require('./models');
 
 const server = new ApolloServer({
   schema,
+  introspection: ENVIRONMENT.DEVELOPMENT,
   context: async ({ req }) => {
     const auth = req ? req.headers.authorization : null;
     if (auth && auth.toLowerCase().startsWith('bearer ')) {
       const decodedToken = jwt.verify(
         auth.substring(7), JWT_SECRET
       );
-      //const currentUser = await User.findById(decodedToken.id);
-      return { decodedToken };
+      const currentUser = await Account.findByPk(decodedToken.id);
+      return { currentUser };
     }
   }
 });
