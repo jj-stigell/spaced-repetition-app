40c0c8489a6353cbf1526a6a77de5fe5418a6f1e
Jere Stigell <jj.stigell@gmail.com>
2022-10-08 17:49:42 +0900

    add if exists

--- setup/database/dropTables.sql
+++ setup/database/dropTables.sql
@@ -1,15 +1,15 @@
 -- For purging the whole database
 BEGIN;
-DROP TYPE result CASCADE;
-DROP TABLE account CASCADE;
-DROP TABLE country CASCADE;
-DROP TABLE example_word CASCADE;
-DROP TABLE kanji CASCADE;
-DROP TABLE radical CASCADE;
-DROP TABLE radical_translation CASCADE;
-DROP TABLE kanji_radical CASCADE;
-DROP TABLE example_word_translation CASCADE;
-DROP TABLE translation_kanji CASCADE;
-DROP TABLE account_kanji_review CASCADE;
-DROP TABLE account_kanji_card CASCADE;
+DROP TYPE IF EXISTS result CASCADE;
+DROP TABLE IF EXISTS account CASCADE;
+DROP TABLE IF EXISTS country CASCADE;
+DROP TABLE IF EXISTS example_word CASCADE;
+DROP TABLE IF EXISTS kanji CASCADE;
+DROP TABLE IF EXISTS radical CASCADE;
+DROP TABLE IF EXISTS radical_translation CASCADE;
+DROP TABLE IF EXISTS kanji_radical CASCADE;
+DROP TABLE IF EXISTS example_word_translation CASCADE;
+DROP TABLE IF EXISTS translation_kanji CASCADE;
+DROP TABLE IF EXISTS account_kanji_review CASCADE;
+DROP TABLE IF EXISTS account_kanji_card CASCADE;
 COMMIT;
