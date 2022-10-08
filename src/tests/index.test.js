b98f772960396b5bd80095719e9d6af9e484d9e1
Jere Stigell <jj.stigell@gmail.com>
2022-10-08 15:48:44 +0900

    setup for testing

---
+++ src/tests/index.test.js
@@ -0,0 +1,14 @@
+
+const {expect, test} = require('@jest/globals');
+
+test('reverse of a', () => {
+  expect('a').toBe('a');
+});
+
+test('reverse of react', () => {
+  expect('tcaer').toBe('tcaer');
+});
+
+test('reverse of releveler', () => {
+  expect('releveler').toBe('releveler');
+});
