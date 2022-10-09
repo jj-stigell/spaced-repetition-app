dbfb698378b3e7a96e2a098781e0df5bdc972c6e
Jere Stigell <jj.stigell@gmail.com>
2022-10-09 22:41:52 +0900

    return at prod rollback

--- src/util/rollback.js
+++ src/util/rollback.js
@@ -3,6 +3,7 @@ const { ENVIRONMENT, NODE_ENV } = require('./config');
 
 if (ENVIRONMENT.PRODUCTION) {
   console.log('Be careful when rolling back, this is production version');
+  return;
 } else {
   console.log('Rolling back on:', NODE_ENV);
   rollbackMigration();
