93bca1324f1f069680ede00bfbee80b80db53c9d
Jere Stigell <jj.stigell@gmail.com>
2022-10-14 23:22:50 +0900

    add new errors

--- src/util/errors.js
+++ src/util/errors.js
@@ -1,5 +1,6 @@
 const errors = {
   inputValueMissingError: 'inputValueMissingError',
+  inputValueTypeError: 'inputValueTypeError',
   passwordMismatchError: 'passwordMismatchError',
   passwordValidationError: 'passwordValidationError',
   notEmailError: 'notEmailError',
@@ -12,6 +13,9 @@ const errors = {
   changePasswordValueMissingError: 'changePasswordValueMissingError',
   currAndNewPassEqualError: 'currAndNewPassEqualError',
   currentPasswordIncorrect: 'currentPasswordIncorrect',
+  invalidJlptLevelError: 'invalidJlptLevelError',
+  invalidLanguageIdError: 'invalidLanguageIdError',
+  limitReviewsRangeError: 'limitReviewsRangeError'
 };
 
 module.exports = errors;
