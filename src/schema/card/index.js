329636f68978538ad7ad789ef1f6ae2b83073bd8
Jere Stigell <jj.stigell@gmail.com>
2022-10-15 22:40:13 +0900

    new cards query building

--- src/schema/card/index.js
+++ src/schema/card/index.js
@@ -4,6 +4,9 @@ const validator = require('validator');
 const { Kanji, AccountKanjiReview, AccountKanjiCard, TranslationKanji, Radical, TranslationRadical } = require('../../models');
 const errors = require('../../util/errors');
 const constants = require('../../util/constants');
+const { sequelize } = require('../../util/database');
+
+const Model = require('sequelize/lib/model');
 
 const typeDef = `
   type Account {
@@ -73,15 +76,45 @@ const typeDef = `
     radicals: [RadicalData]
   }
 
+  type NewCard {
+    id: Int
+    kanji: String
+    learningOrder: Int
+    jlptLevel: Int
+    onyomi: String
+    onyomiRomaji: String
+    kunyomi: String
+    kunyomiRomaji: String
+    strokeCount: Int
+    createdAt: String
+    updatedAt: String
+    translation_kanjis: TranslationKanjiData
+    radicals: RadicalData
+  }
+
+  type NewCardSet {
+    NewCards: [NewCard]
+  }
+
   type CardSet {
     Cards: [Card]
   }
 
+  
+
   union CardPayload = CardSet | Error
+  union NewCardPayload = NewCardSet | Error
   union RescheduleResult = Success | Error
 
   type Query {
     fetchDueKanjiCards(
+      jlptLevel: Int
+      includeLowerLevelCards: Boolean
+      limitReviews: Int
+      langId: String
+    ): CardPayload!
+
+    fetchNewKanjiCards(
       jlptLevel: Int
       includeLowerLevelCards: Boolean
       limitReviews: Int
@@ -206,6 +239,135 @@ const resolvers = {
       return {
         __typename: 'CardSet',
         Cards: cards,
+      };
+    },
+    // Fetch cards that are due or new cards based on the newCards boolean value, defaults to false.
+    fetchNewKanjiCards: async (_, { jlptLevel, includeLowerLevelCards, limitReviews, langId }, { currentUser }) => {
+
+      // Check that user is logged in
+      if (!currentUser) {
+        return { 
+          __typename: 'Error',
+          errorCode: errors.notAuthError
+        };
+      }
+
+      // Confirm that jlpt level and language id are not empty
+      if (!jlptLevel || !langId) {
+        return { 
+          __typename: 'Error',
+          errorCode: errors.inputValueMissingError
+        };
+      }
+
+      // Chack that language id is one of the available
+      if (!validator.isIn(langId, constants.availableLanguages)) {
+        return { 
+          __typename: 'Error',
+          errorCode: errors.invalidLanguageIdError
+        };
+      }
+
+      // Check that type of integer correct
+      if (!Number.isInteger(jlptLevel) || (limitReviews && !Number.isInteger(limitReviews))) {
+        return { 
+          __typename: 'Error',
+          errorCode: errors.inputValueTypeError
+        };
+      }
+
+      // Check that jlpt level between 1 - 5
+      if (!constants.jltpLevels.includes(jlptLevel)) {
+        return { 
+          __typename: 'Error',
+          errorCode: errors.invalidJlptLevelError
+        };
+      }
+
+      // Check that limitReviews in correct range (1 - 9999)
+      if (limitReviews > 9999 || limitReviews < 1) {
+        return { 
+          __typename: 'Error',
+          errorCode: errors.limitReviewsRangeError
+        };
+      }
+
+      let selectLevel = { [Op.eq]: jlptLevel };
+
+      // Set where filter to JLPT level >= jlptLevel, lower level cards included
+      if (includeLowerLevelCards) {
+        selectLevel = { [Op.gte]: jlptLevel };
+      }
+
+      // fetch id of new cards (cards not existing in user cards)
+      const rawQuery = `SELECT id FROM kanji WHERE jlpt_level = :jlptLevel AND NOT EXISTS (
+        SELECT NULL 
+        FROM account_kanji_card 
+        WHERE account_kanji_card.account_id = :accountId AND kanji.id = account_kanji_card.kanji_id
+      ) 
+      ORDER BY learning_order ASC LIMIT :limitReviews`;
+
+      const cardIds = await sequelize.query(rawQuery, {
+        replacements: {
+          jlptLevel: jlptLevel,
+          accountId: currentUser.id,
+          limitReviews: limitReviews,
+        },
+        model: Kanji,
+        type: sequelize.QueryTypes.SELECT,
+        raw: true
+      });
+
+      const ids = cardIds.map(card => card.id);
+
+      console.log(ids);
+
+      const cards = await Kanji.findAll({
+        where: {
+          'jlptLevel': selectLevel
+        },
+        include: [
+          {
+            model: AccountKanjiCard,
+            attributes: ['reviewCount', 'easyFactor', 'accountStory', 'accountHint', 'dueDate'],
+            where: {
+              account_id: currentUser.id
+            }
+          },
+          {
+            model: TranslationKanji,
+            attributes: ['keyword', 'story', 'hint', 'otherMeanings'],
+            where: {
+              language_id: langId
+            }
+          },
+          {
+            model: Radical,
+            attributes: ['id', 'radical', 'reading', 'readingRomaji', 'strokeCount', 'createdAt', 'updatedAt'],
+            include: {
+              model: TranslationRadical,
+              where: {
+                language_id: langId
+              }
+            },
+          },
+        ],
+        order: [
+          [AccountKanjiCard, 'dueDate', 'ASC']
+        ]
+      });
+
+
+      if (limitReviews) {
+        return {
+          __typename: 'CardSet',
+          Cards: cardIds.slice(0, limitReviews),
+        };
+      }
+
+      return {
+        __typename: 'CardSet',
+        Cards: cardIds,
       };
     },
   },
