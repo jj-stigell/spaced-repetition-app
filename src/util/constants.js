1079a46d0e5cf40b81fdc5626dbd63e6dd152cb0
Jere Stigell <jj.stigell@gmail.com>
2022-10-16 13:16:06 +0900

    add constant for available results

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,6 +1,7 @@
 const constants = {
   matureInterval: 21,
   availableLanguages: ['en', 'fi'],
+  availableResults: ['again', 'hard', 'easy'],
   jltpLevels: [1, 2, 3, 4, 5],
   maxLimitReviews: 9999,
   minLimitReviews: 1
