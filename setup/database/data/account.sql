a4bc363d94deb96bd137e4a8a25309671a099b8f
Jere Stigell <jj.stigell@gmail.com>
2022-10-20 16:54:20 +0900

    language_id

--- setup/database/data/account.sql
+++ setup/database/data/account.sql
@@ -1,17 +1,17 @@
 INSERT INTO account (
-  email, email_verified, password_hash, member, language, last_login, created_at, updated_at
+  email, email_verified, password_hash, member, language_id, last_login, created_at, updated_at
 ) VALUES (
     'admin@test.com', false, '$2b$10$GE7OUAW2u2u0WDiJ8nBxIucrIYSV8ZSBU0PCoV.Heg9C5pLnFdqZu', true, 'en', NOW(), NOW(), NOW()
 );
 
 INSERT INTO account (
-  email, email_verified, password_hash, member, language, last_login, created_at, updated_at
+  email, email_verified, password_hash, member, language_id, last_login, created_at, updated_at
 ) VALUES (
     'en@test.com', false, '$2b$10$GE7OUAW2u2u0WDiJ8nBxIucrIYSV8ZSBU0PCoV.Heg9C5pLnFdqZu', true, 'en', NOW(), NOW(), NOW()
 );
 
 INSERT INTO account (
-  email, email_verified, password_hash, member, language, last_login, created_at, updated_at
+  email, email_verified, password_hash, member, language_id, last_login, created_at, updated_at
 ) VALUES (
     'fi@test.com', false, '$2b$10$GE7OUAW2u2u0WDiJ8nBxIucrIYSV8ZSBU0PCoV.Heg9C5pLnFdqZu', true, 'en', NOW(), NOW(), NOW()
 );
