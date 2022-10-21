a6d6aa1835eacb0444dc4b3fd9e67ea1684cedda
Jere Stigell <jj.stigell@gmail.com>
2022-10-21 17:34:20 +0900

    add constants

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,14 +1,19 @@
 const constants = {
   matureInterval: 21,
+  defaultLanguage: 'en',
   availableLanguages: ['en', 'fi'],
   resultTypes: ['again', 'good'],
   deckTypes: ['recall', 'recognise'],
   cardTypes: ['kanji', 'hiragana', 'katakana', 'japanese_word', 'japanese_sentence'],
   jltpLevels: [1, 2, 3, 4, 5],
   maxLimitReviews: 999,
-  minLimitReviews: 1,
+  minLimitReviews: 0,
   maxReviewInterval: 999,
   minReviewInterval: 1,
+
+  maxNewReviews: 100,
+  minNewReviews: 0,
+
   defaultMaxInterval: 999,
   defaultMaxReviewPerDay: 999,
   defaultMaxNewPerDay: 15
