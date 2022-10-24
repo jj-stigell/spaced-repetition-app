0579a707549f7e9763395bc07f098341d0bbdfed
Jere Stigell <jj.stigell@gmail.com>
2022-10-24 23:20:21 +0900

    due cards id raw query

--- src/schema/card/rawQueries.js
+++ src/schema/card/rawQueries.js
@@ -1,9 +1,15 @@
-const selectNewCards = `SELECT card_id FROM card_list WHERE deck_id = :deckId AND NOT EXISTS (
+const selectNewCardIds = `SELECT card_id FROM card_list WHERE deck_id = :deckId AND NOT EXISTS (
   SELECT NULL 
   FROM account_card 
   WHERE account_card.account_id = :accountId AND card_list.card_id = account_card.card_id
 ) ORDER BY learning_order ASC LIMIT :limitReviews`;
 
+const selectDueCardIds = `SELECT card_list.card_id FROM card_list INNER JOIN 
+account_card ON account_card.card_id = card_list.card_id 
+WHERE card_list.deck_id = :deckId AND account_card.account_id = :accountId 
+ORDER BY due_at ASC LIMIT :limitReviews`;
+
 module.exports = {
-  selectNewCards
+  selectNewCardIds,
+  selectDueCardIds
 };
