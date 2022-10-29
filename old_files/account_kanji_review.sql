518a49e2b7102801226155fa2e32c334da23ee80
Jere Stigell <jj.stigell@gmail.com>
2022-10-29 16:14:20 +0900

    delete old files

--- old_files/account_kanji_review.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO account_kanji_review (account_id, kanji_id, review_result) VALUES (1, 1, 'easy');
-INSERT INTO account_kanji_review (account_id, kanji_id, review_result) VALUES (1, 1, 'hard');
-INSERT INTO account_kanji_review (account_id, kanji_id, review_result) VALUES (1, 1, 'again');
-INSERT INTO account_kanji_review (account_id, kanji_id, review_result) VALUES (1, 2, 'easy');
-INSERT INTO account_kanji_review (account_id, kanji_id, review_result) VALUES (1, 3, 'easy');
