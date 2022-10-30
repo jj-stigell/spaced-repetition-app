8110a1badb62516fa835830a3a543f7692205d97
Jere Stigell <jj.stigell@gmail.com>
2022-10-30 18:52:52 +0900

    push cards in deck X

--- src/database/rawQueries.js
+++ src/database/rawQueries.js
@@ -13,9 +13,16 @@ const findCard = 'SELECT 1 FROM card WHERE id = :cardId';
 
 const pushAllCardsNDays = 'UPDATE account_card SET due_at = due_at + :days WHERE account_id = :accountId';
 
+const pushCardsInDeckIdNDays = `UPDATE account_card SET due_at = account_card.due_at + :days FROM
+  (SELECT card_list.card_id FROM card_list 
+  INNER JOIN account_card ON account_card.card_id = card_list.card_id 
+  where deck_id = :deckId AND account_id = :accountId)
+  AS found WHERE found.card_id = account_card.card_id`;
+
 module.exports = {
   selectNewCardIds,
   selectDueCardIds,
   findCard,
-  pushAllCardsNDays
+  pushAllCardsNDays,
+  pushCardsInDeckIdNDays
 };
