fd912dceeeeb5988bed18db1c3fdb134ab2f2cde
Jere Stigell <jj.stigell@gmail.com>
2022-10-31 20:33:41 +0900

    import kanji schema

--- src/schema/index.js
+++ src/schema/index.js
@@ -3,10 +3,11 @@ const { merge } = require('lodash');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { typeDef: Account, resolvers: accountResolvers } = require('./account/');
 const { typeDef: Card, resolvers: cardResolvers } = require('./card/');
+const { typeDef: Kanji, resolvers: kanjiResolvers } = require('./kanji/');
 
 const schema = makeExecutableSchema({
-  typeDefs: [ Account, Card ],
-  resolvers: merge(accountResolvers, cardResolvers),
+  typeDefs: [ Account, Card, Kanji ],
+  resolvers: merge(accountResolvers, cardResolvers, kanjiResolvers),
 });
 
 module.exports = schema;
