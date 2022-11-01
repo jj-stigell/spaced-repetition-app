61d3911d89bc85f2c36e757838cb47de45ce4985
Jere Stigell <jj.stigell@gmail.com>
2022-11-01 23:34:54 +0900

    update test queries and mutations

--- src/tests/utils/mutations.js
+++ src/tests/utils/mutations.js
@@ -1,39 +1,24 @@
 const mutations = {
   registerMutation: `mutation createAccount($email: String!, $password: String!, $passwordConfirmation: String!, $languageId: String) {
     createAccount(email: $email, password: $password, passwordConfirmation: $passwordConfirmation, languageId: $languageId) {
-      ... on Error {
-        errorCodes
-      }
-      ... on Account {
-        id
-        email
-      }
+      id
+      email
     }
   }`,
   loginMutation: `mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      ... on Error {
-        errorCodes
+      token {
+        value
       }
-      ... on AccountToken {
-        token {
-          value
-        }
-        user {
-          id,
-          email
-        }
+      user {
+        id,
+        email
       }
     }
   }`,
   changePasswordMutation: `mutation changePassword($currentPassword: String!, $newPassword: String!, $newPasswordConfirmation: String!) {
     changePassword(currentPassword: $currentPassword, newPassword: $newPassword, newPasswordConfirmation: $newPasswordConfirmation) {
-      ... on Error {
-        errorCodes
-      }
-      ... on Success {
-        status
-      }
+      status
     }
   }`,
 };
--- src/tests/utils/queries.js
+++ src/tests/utils/queries.js
@@ -1,12 +1,7 @@
 const queries = {
   emailAvailableQuery: `query emailAvailable($email: String!) {
     emailAvailable(email: $email) {
-      ... on Error {
-        errorCodes
-      }
-      ... on Success {
-        status
-      }
+      status
     }
   }`,
 };
