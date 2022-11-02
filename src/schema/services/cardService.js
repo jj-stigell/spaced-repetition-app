3a64a0990e360c96540a1e3d3cdf57957b1f672a
Jere Stigell <jj.stigell@gmail.com>
2022-11-02 11:14:43 +0900

    find reviews and dues

--- src/schema/services/cardService.js
+++ src/schema/services/cardService.js
@@ -1,6 +1,8 @@
 const { internalServerError } = require('../../util/errors/graphQlErrors');
 //const constants = require('../../util/constants');
 const models = require('../../models');
+const { sequelize } = require('../../database');
+const rawQueries = require('./rawQueries');
 
 const findAllDecks = async (includeInactive) => {
   try {
@@ -34,6 +36,42 @@ const findAllDecks = async (includeInactive) => {
   }
 };
 
+const findReviewHistory = async (limitReviews, accountId) => {
+  try {
+    return await sequelize.query(rawQueries.fetchDailyReviewHistoryNDays, {
+      replacements: {
+        limitReviews: limitReviews,
+        accountId: accountId
+      },
+      model: models.AccountReview,
+      type: sequelize.QueryTypes.SELECT,
+      raw: true
+    });
+  } catch (error) {
+    console.log(error);
+    return internalServerError();
+  }
+};
+
+const findDueReviewsCount = async (limitReviews, accountId) => {
+  try {
+    return await sequelize.query(rawQueries.fetchDueReviewsNDays, {
+      replacements: {
+        limitReviews: limitReviews,
+        accountId: accountId
+      },
+      model: models.AccountCard,
+      type: sequelize.QueryTypes.SELECT,
+      raw: true
+    });
+  } catch (error) {
+    console.log(error);
+    return internalServerError();
+  }
+};
+
 module.exports = {
-  findAllDecks
+  findAllDecks,
+  findReviewHistory,
+  findDueReviewsCount
 };
