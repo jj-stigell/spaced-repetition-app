798aa54f923ee869d2721de091b44763f28ab7ad
Jere Stigell <jj.stigell@gmail.com>
2022-11-05 14:28:46 +0900

    add card services

--- src/schema/services/cardService.js
+++ src/schema/services/cardService.js
@@ -1,6 +1,7 @@
 const { internalServerError } = require('../../util/errors/graphQlErrors');
 const models = require('../../models');
 const { sequelize } = require('../../database');
+const { Op } = require('sequelize');
 const constants = require('../../util/constants');
 const rawQueries = require('./rawQueries');
 
@@ -41,6 +42,175 @@ const updateAccountCard = async (cardId, accountId, story, hint) => {
       accountStory: story ? story : null,
       accountHint: hint ? hint : null
     });
+  } catch (error) {
+    console.log(error);
+    return internalServerError();
+  }
+};
+
+/**
+ * Add new row to review history
+ * @param {*} cardId 
+ * @param {*} accountId 
+ * @param {*} reviewResult 
+ * @param {*} extraReview 
+ * @param {*} timing 
+ * @returns 
+ */
+const createAccountReview = async (cardId, accountId, reviewResult, extraReview, timing) => {
+  try {
+    return await models.AccountReview.create({
+      accountId: accountId,
+      cardId: cardId,
+      result: reviewResult,
+      extraReview: extraReview ? true : false,
+      timing: timing
+    });
+  } catch (error) {
+    console.log(error);
+    return internalServerError();
+  }
+};
+
+const fetchNewCards = async (deckId, accountId, limitReviews, selectedLanguage) => {
+  try {
+    const cardIds = await sequelize.query(rawQueries.selectNewCardIds, {
+      replacements: {
+        deckId: deckId,
+        accountId: accountId,
+        limitReviews: limitReviews,
+      },
+      model: models.CardList,
+      type: sequelize.QueryTypes.SELECT,
+      raw: true
+    });
+
+    const idInLearningOrder = cardIds.map(listItem => listItem.card_id);
+
+    const cards = await models.Card.findAll({
+      where: {
+        'id': { [Op.in]: idInLearningOrder },
+        'active': true
+      },
+      subQuery: false,
+      nest: true,
+      include: [
+        {
+          model: models.Kanji,
+          include: [
+            {
+              model: models.KanjiTranslation,
+              where: {
+                language_id: selectedLanguage
+              },
+            },
+            {
+              model: models.Radical,
+              attributes: ['id', 'radical', 'reading', 'readingRomaji', 'strokeCount', 'createdAt', 'updatedAt'],
+              include: {
+                model: models.RadicalTranslation,
+                where: {
+                  language_id: selectedLanguage
+                }
+              },
+            }
+          ]
+        },
+        {
+          model: models.Word,
+          include: {
+            model: models.WordTranslation,
+            where: {
+              language_id: selectedLanguage
+            },
+          }
+        }
+      ]
+    });
+
+    if (cardIds.length === 0) return null;
+
+    cards.sort(function (a, b) {
+      return idInLearningOrder.indexOf(a.id) - idInLearningOrder.indexOf(b.id);
+    });
+
+    return cards;
+  } catch (error) {
+    console.log(error);
+    return internalServerError();
+  }
+};
+
+const fetchDueCards = async (deckId, accountId, limitReviews, selectedLanguage) => {
+  try {
+
+    const cardIds = await sequelize.query(rawQueries.selectDueCardIds, {
+      replacements: {
+        deckId: deckId,
+        accountId: accountId,
+        limitReviews: limitReviews,
+      },
+      model: models.CardList,
+      type: sequelize.QueryTypes.SELECT,
+      raw: true
+    });
+
+    const idInLearningOrder = cardIds.map(listItem => listItem.card_id);
+
+    const cards = await models.Card.findAll({
+      where: {
+        'id': { [Op.in]: idInLearningOrder },
+        'active': true
+      },
+      subQuery: false,
+      nest: true,
+      include: [
+        {
+          model: models.Kanji,
+          include: [
+            {
+              model: models.KanjiTranslation,
+              where: {
+                language_id: selectedLanguage
+              },
+            },
+            {
+              model: models.Radical,
+              attributes: ['id', 'radical', 'reading', 'readingRomaji', 'strokeCount', 'createdAt', 'updatedAt'],
+              include: {
+                model: models.RadicalTranslation,
+                where: {
+                  language_id: selectedLanguage
+                }
+              },
+            }
+          ]
+        },
+        {
+          model: models.AccountCard,
+          where: {
+            accountId: accountId
+          }
+        },
+        {
+          model: models.Word,
+          include: {
+            model: models.WordTranslation,
+            where: {
+              language_id: selectedLanguage
+            },
+          }
+        }
+      ]
+    });
+
+    if (cardIds.length === 0) return null;
+
+    cards.sort(function (a, b) {
+      return idInLearningOrder.indexOf(a.id) - idInLearningOrder.indexOf(b.id);
+    });
+
+    return cards;
   } catch (error) {
     console.log(error);
     return internalServerError();
@@ -134,6 +304,9 @@ module.exports = {
   findCardById,
   createAccountCard,
   updateAccountCard,
+  createAccountReview,
+  fetchNewCards,
+  fetchDueCards,
   findReviewHistory,
   findDueReviewsCount,
   pushAllCards,
