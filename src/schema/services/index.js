8250c50911360b820a95ad0ed67ce05332f3c754
Jere Stigell <jj.stigell@gmail.com>
2022-11-15 07:40:30 +0900

    export all services through index.js

---
+++ src/schema/services/index.js
@@ -0,0 +1,15 @@
+const accountService = require('./accountService');
+const cardService = require('./cardService');
+const deckService = require('./accountService');
+const kanjiService = require('./kanjiService');
+const sessionService = require('./sessionService');
+const bugService = require('./bugService');
+
+module.exports = {
+  accountService,
+  cardService,
+  deckService,
+  kanjiService,
+  sessionService,
+  bugService
+};
