8f6311c8dc21fe5a5eaceeddff3943951965cd9a
Jere Stigell <jj.stigell@gmail.com>
2022-11-15 07:32:55 +0900

    bugservice

---
+++ src/schema/services/bugService.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const { internalServerError } = require('../../util/errors/graphQlErrors');
+const constants = require('../../util/constants');
+const models = require('../../models');
+
+const findBugReportById = async (bugId) => {
+  try {
+    return await models.BugReport.findByPk(bugId);
+  } catch (error) {
+    return internalServerError(error);
+  }
+};
+
+const findAllBugReports = async () => {
+  try {
+    return await models.BugReport.findAll({});
+  } catch (error) {
+    return internalServerError(error);
+  }
+};
+
+const createNewBugReport = async (email, passwordHash) => {
+  try {
+    return await models.BugReport.create({
+      email: email.toLowerCase(),
+      passwordHash: passwordHash,
+    });
+  } catch (error) {
+    return internalServerError(error);
+  }
+};
+
+module.exports = {
+  findBugReportById,
+  findAllBugReports,
+  createNewBugReport
+};
