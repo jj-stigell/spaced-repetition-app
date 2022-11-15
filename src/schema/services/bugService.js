cfd20ca6b8b5660313017cefcaaca2d63045ce2a
Jere Stigell <jj.stigell@gmail.com>
2022-11-15 09:48:24 +0900

    comments

--- src/schema/services/bugService.js
+++ src/schema/services/bugService.js
@@ -1,9 +1,11 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 const { internalServerError } = require('../../util/errors/graphQlErrors');
-const constants = require('../../util/constants');
 const models = require('../../models');
 
+/**
+ * Find specific bug report by id (PK)
+ * @param {integer} id of the bug report
+ * @returns {BugReport} Bug report object
+ */
 const findBugReportById = async (id) => {
   try {
     return await models.BugReport.findByPk(id);
@@ -12,6 +14,10 @@ const findBugReportById = async (id) => {
   }
 };
 
+/**
+ * Find all bug reports, both solved and unsolved
+ * @returns {Array<models.BugReport>} An array of all bug reports
+ */
 const findAllBugReports = async () => {
   try {
     return await models.BugReport.findAll({});
@@ -20,6 +26,11 @@ const findAllBugReports = async () => {
   }
 };
 
+/**
+ * Find all bug reports by type
+ * @param {string} type Type of the bug, possible types defined in constants file
+ * @returns {Array<models.BugReport>} An array of all bug reports of type
+ */
 const findAllBugReportsByType = async (type) => {
   try {
     return await models.BugReport.findAll({
@@ -32,6 +43,14 @@ const findAllBugReportsByType = async (type) => {
   }
 };
 
+/**
+ * Create new bug report
+ * @param {string} type Type of the bug, possible types defined in constants file 
+ * @param {string} message Message to describe the bug
+ * @param {integer} accountId accounts id number
+ * @param {integer} cardId card id, if bug connected to certain card, can be null
+ * @returns Newly created bug report
+ */
 const createNewBugReport = async (type, message, accountId, cardId) => {
   try {
     return await models.BugReport.create({
@@ -45,6 +64,13 @@ const createNewBugReport = async (type, message, accountId, cardId) => {
   }
 };
 
+/**
+ * Admin solve/edit bug report
+ * @param {integer} bugId Id of the bug report
+ * @param {string} solvedMessage Message describing how bug was handled
+ * @param {boolean} solved true if bug fixed, false otherwise, no null values
+ * @returns Edited bug report
+ */
 const solveBugReport = async (bugId, solvedMessage, solved) => {
   try {
     const bugReport = await findBugReportById(bugId);
