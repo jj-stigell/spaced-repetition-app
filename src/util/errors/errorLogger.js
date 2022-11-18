feab1471b95e355a357c3f293ed93d9cacfe2ba7
Jere Stigell <jj.stigell@gmail.com>
2022-11-19 04:53:24 +0900

    no error loggin in tests

--- src/util/errors/errorLogger.js
+++ src/util/errors/errorLogger.js
@@ -1,5 +1,7 @@
+const { NODE_ENV } = require('../config');
+
 const errorLogger = (...params) => {
-  console.error(...params);
+  if(NODE_ENV !== 'test') console.error(...params);
 };
 
 module.exports = errorLogger;
