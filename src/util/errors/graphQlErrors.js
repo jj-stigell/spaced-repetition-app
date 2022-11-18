457496f37f693144f0221ad87056ee04c133d6e2
Jere Stigell <jj.stigell@gmail.com>
2022-11-19 04:59:52 +0900

    comments and notauthorized error

--- src/util/errors/graphQlErrors.js
+++ src/util/errors/graphQlErrors.js
@@ -2,6 +2,10 @@ const { GraphQLError } = require('graphql');
 const errors = require('./errors');
 const errorLogger = require('./errorLogger');
 
+/**
+ * Thrown at situations were authentication needed but not provided
+ * @throws {GraphQLError}
+ */
 const notAuthError = () => {
   errorLogger('not authenticated');
   throw new GraphQLError(errors.graphQlErrors.unauthenticated, {
@@ -12,6 +16,26 @@ const notAuthError = () => {
   });
 };
 
+/**
+ * Error raised if accessing authorized area without authorization
+ * @param {string} error message included in the authorization error
+ * @throws {GraphQLError}
+ */
+const notAuthorizedError = (error) => {
+  errorLogger('not authorized, error(s)', error);
+  throw new GraphQLError(errors.graphQlErrors.unauthorized, {
+    extensions: {
+      code: error,
+      http: { status: 401 } // 401 Unauthorized
+    },
+  });
+};
+
+/**
+ * Raised during database calls
+ * @param {object} error error encountered, not returned to the client
+ *  @throws {GraphQLError}
+ */
 const internalServerError = (error) => {
   errorLogger(error);
   throw new GraphQLError(errors.graphQlErrors.internalServerError, {
@@ -22,6 +46,12 @@ const internalServerError = (error) => {
   });
 };
 
+/**
+ * Extends the graphql own validation with yup
+ * return all yup error encountered during validation
+ * @param {Array<string>} validationErrors array of error code strings
+ * @throws {GraphQLError}
+ */
 const validationError = (validationErrors) => {
   errorLogger(validationErrors);
   throw new GraphQLError(errors.graphQlErrors.validationError, {
@@ -32,6 +62,11 @@ const validationError = (validationErrors) => {
   });
 };
 
+/**
+ * Error for general cases, like email taken, password mismatch etc.
+ * @param {string} error error message
+ * @throws {GraphQLError}
+ */
 const defaultError = (error) => {
   errorLogger(error);
   throw new GraphQLError(errors.graphQlErrors.defaultError, {
@@ -44,6 +79,7 @@ const defaultError = (error) => {
 
 module.exports = {
   notAuthError,
+  notAuthorizedError,
   internalServerError,
   validationError,
   defaultError
