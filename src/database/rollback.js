99427a218c879f8a085afcf2da6cc60a939055de
Jere Stigell <jj.stigell@gmail.com>
2022-12-03 00:44:23 +0900

    rollback confirmation for production

--- src/database/rollback.js
+++ src/database/rollback.js
@@ -1,9 +1,24 @@
 const { rollbackMigration } = require('.');
 const { NODE_ENV } = require('../util/config');
 
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 if (NODE_ENV === 'production') {
-  console.log('Be careful when rolling back, this is production version');
-  return;
+  console.log('\x1b[31m%s\x1b[0m', 'Be careful when rolling back, this is production version');
+  readline.question('Type \x1b[41m ROLLBACK \x1b[0m to rollback production:', input => {
+    if (input === 'ROLLBACK') {
+      console.log('Rolling back one migration');
+      readline.close();
+      rollbackMigration();
+      return;
+    }
+    console.log('Rolling back canceled');
+    readline.close();
+    return;
+  });
 } else {
   console.log('Rolling back on:', NODE_ENV);
   rollbackMigration();
