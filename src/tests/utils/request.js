a0c52df4512cdb859f7fffac4471a01cb194396f
Jere Stigell <jj.stigell@gmail.com>
2022-12-13 18:51:39 +0900

    add queries for testing

---
+++ src/tests/utils/request.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+
+/**
+ * Send a query request with payload to the test server
+ * @param {string} testUrl - url for the server running the backend
+ * @param {string} token - authorization token for accessing the server
+ * @param {string} query - graphql query
+ * @param {object} data - objevt containing the data for the query
+ * @returns 
+ */
+const sendRequest = async (testUrl, token, query, payload) => {
+  if (token) {
+    return await request(testUrl)
+      .post('/')
+      .set('Authorization', `bearer ${token}`)
+      .send({ query: query, variables: payload });
+  }
+  return await request(testUrl)
+    .post('/')
+    .send({ query: query, variables: payload });
+};
+
+module.exports = sendRequest;
