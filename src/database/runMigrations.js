7a27abb48fdf84299ebbadc04481f8cae2e9acbf
Jere Stigell <jj.stigell@gmail.com>
2022-12-15 09:17:16 +0900

    fix typo

--- src/database/runMigrations.js
+++ src/database/runMigrations.js
@@ -2,7 +2,7 @@ const { runMigrations } = require('.');
 const { NODE_ENV } = require('../util/config');
 
 try {
-  console.log('Running seeds on:', NODE_ENV);
+  console.log('Running migrations on:', NODE_ENV);
   runMigrations();
 } catch (error) {
   console.log(error);
