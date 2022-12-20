65253e69926943d96ee9eaf6c4b81459a8766ac3
Jere Stigell <jj.stigell@gmail.com>
2022-12-20 20:55:10 +0900

    fix comments

--- src/util/errors/errorLogger.js
+++ src/util/errors/errorLogger.js
@@ -1,8 +1,8 @@
 const { NODE_ENV } = require('../config');
 
 /**
- * Log errors from functions
- * @param  {...any} params object, string, array of errors
+ * Log errors from functions.
+ * @param  {...any} params - object, string, array of errors
  */
 const errorLogger = (...params) => {
   if(NODE_ENV !== 'test') console.error(...params);
