2cc062e71ccd7d41aa6853df3a9a1b99adbf40c1
Jere Stigell <jj.stigell@gmail.com>
2023-04-25 18:15:31 +0300

    update guards

--- client/src/utils/route-guard/GuestGuard.tsx
+++ client/src/utils/route-guard/GuestGuard.tsx
@@ -6,13 +6,14 @@ import { Outlet, Navigate } from 'react-router-dom'
 // Project imports
 import { useAppSelector } from '../../app/hooks'
 import { RootState } from '../../app/store'
+import { dashboard } from '../../config/path'
 
 function GuestGuard (): JSX.Element {
-  const { isLoggedIn }: { isLoggedIn: boolean } = useAppSelector((state: RootState) => state.account)
+  const isLoggedIn: boolean = useAppSelector((state: RootState) => state.account.isLoggedIn)
 
   if (!isLoggedIn) return <Outlet/>
 
-  return <Navigate to="/"/>
+  return <Navigate to={dashboard}/>
 }
 
 export default GuestGuard
