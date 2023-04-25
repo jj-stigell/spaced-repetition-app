cf1c5aeefd44364b4fb111ec1b3b842f267f0c46
Jere Stigell <jj.stigell@gmail.com>
2023-04-25 18:16:08 +0300

    add axios

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,3 +1,13 @@
-// define axios here
+import axios from 'axios'
+import { API_SERVER } from '../config/environment'
 
-export {}
+export default axios.create({
+  baseURL: API_SERVER,
+  timeout: 10000,
+  withCredentials: true,
+  responseType: 'json',
+  responseEncoding: 'utf8',
+  headers: {
+    'Content-type': 'application/json'
+  }
+})
