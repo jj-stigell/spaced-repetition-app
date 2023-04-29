a9bcba3c5f44e3ecb7b1fdd9a7258f0750511d60
Jere Stigell <jj.stigell@gmail.com>
2023-04-29 14:56:11 +0300

    update data row

--- server/dbBaseData/deck.sql
+++ server/dbBaseData/deck.sql
@@ -4,4 +4,4 @@ INSERT INTO deck (deck_name, member_only, category, jlpt_level, language_id, act
 ('JLPT N5 Kanji Pack 3 (Kanji 41 to 60)', false, 'KANJI', 5, 'JP', true, NOW(), NOW()),
 ('JLPT N5 Kanji Pack 4 (Kanji 61 to 80)', false, 'KANJI', 5, 'JP', true, NOW(), NOW()),
 ('JLPT N5 Kanji Pack 5 (Kanji 81 to 103)', false, 'KANJI', 5, 'JP', true, NOW(), NOW()),
-('JLPT N5 Vocabulary', false, 'VOCABULARY', 5, 'JP', true, NOW(), NOW()),
+('JLPT N5 Vocabulary', false, 'VOCABULARY', 5, 'JP', true, NOW(), NOW());
