f37d7bce47f7b1e3d715978e1b050eff66e2423a
Jere Stigell <jj.stigell@gmail.com>
2023-05-01 13:20:59 +0300

    update test helpers

--- server/test/utils/constants.ts
+++ server/test/utils/constants.ts
@@ -10,10 +10,20 @@ export const RESET_PASSWORD_URI: string = '/api/v1/account/password/reset';
 export const CHANGE_PASSWORD_URI: string = '/api/v1/account/password';
 export const CHANGE_JLPT_LEVEL_URI: string = '/api/v1/account/jlpt-level';
 export const BUGREPORT_URI: string = '/api/v1/bug';
+export const CATEGORIES_URI: string = '/api/v1/category';
 
 export const user: RegisterData = {
   email: 'userAccount@test.com',
   username: 'useraccount',
+  password: 'TestPassword123',
+  acceptTos: true,
+  allowNewsLetter: true,
+  language: 'EN'
+};
+
+export const nonMember: RegisterData = {
+  email: 'notMember@test.com',
+  username: 'notMemberAcc',
   password: 'TestPassword123',
   acceptTos: true,
   allowNewsLetter: true,
--- server/test/utils/helpers.ts
+++ server/test/utils/helpers.ts
@@ -9,7 +9,9 @@ import { sequelize } from '../../src/database';
 import models from '../../src/database/models';
 import Account from '../../src/database/models/account';
 import { Role, ApiErrorContent } from '../../src/type';
-import { adminRead, adminWrite, LOGIN_URI, REGISTER_URI, superuser, user } from './constants';
+import {
+  adminRead, adminWrite, LOGIN_URI, REGISTER_URI, superuser, user, nonMember
+} from './constants';
 
 const account: string = fs.readFileSync(
   path.resolve(__dirname, '../../dbBaseData/account.sql'), 'utf8'
@@ -47,19 +49,28 @@ export async function resetDatabase(): Promise<void> {
 }
 
 export async function getCookies():
-Promise<[Array<string>, Array<string>, Array<string>, Array<string>]> {
+Promise<[Array<string>, Array<string>, Array<string>, Array<string>, Array<string>]> {
+  await request.post(REGISTER_URI).send(nonMember);
   await request.post(REGISTER_URI).send(user);
   await request.post(REGISTER_URI).send(adminRead);
   await request.post(REGISTER_URI).send(adminWrite);
   await request.post(REGISTER_URI).send(superuser);
 
-  const normalUser: Account = await models.Account.findOne({
+  const nonMemberUser: Account = await models.Account.findOne({
+    where: {
+      email: nonMember.email
+    }
+  }) as Account;
+  nonMemberUser.set({ emailVerified: true, role: Role.NON_MEMBER });
+  await nonMemberUser.save();
+
+  const memberUser: Account = await models.Account.findOne({
     where: {
       email: user.email
     }
   }) as Account;
-  normalUser.set({ emailVerified: true, role: Role.MEMBER });
-  await normalUser.save();
+  memberUser.set({ emailVerified: true, role: Role.MEMBER });
+  await memberUser.save();
 
   const adminReadAccount: Account = await models.Account.findOne({
     where: {
@@ -85,8 +96,11 @@ Promise<[Array<string>, Array<string>, Array<string>, Array<string>]> {
   superUserAccount.set({ emailVerified: true, role: Role.SUPERUSER });
   await superUserAccount.save();
 
+  res = await request.post(LOGIN_URI).send(nonMember);
+  const nonMemberUserCookies: Array<string> = res.headers['set-cookie'];
+
   res = await request.post(LOGIN_URI).send(user);
-  const userCookies: Array<string> = res.headers['set-cookie'];
+  const memberUserCookies: Array<string> = res.headers['set-cookie'];
 
   res = await request.post(LOGIN_URI).send(adminRead);
   const adminReadCookies: Array<string> = res.headers['set-cookie'];
@@ -97,7 +111,11 @@ Promise<[Array<string>, Array<string>, Array<string>, Array<string>]> {
   res = await request.post(LOGIN_URI).send(superuser);
   const superuserCookies: Array<string> = res.headers['set-cookie'];
 
-  return [userCookies, adminReadCookies, adminWriteCookies, superuserCookies];
+  return [
+    memberUserCookies, adminReadCookies,
+    adminWriteCookies, superuserCookies,
+    nonMemberUserCookies
+  ];
 }
 
 export function checkErrors(errors: Array<ApiErrorContent>, code: string): void {
