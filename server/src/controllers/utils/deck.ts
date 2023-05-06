a1861ca8152650fa439f7f95180e477a8d82ccbc
Jere Stigell <jj.stigell@gmail.com>
2023-05-06 13:44:20 +0300

    deck util

---
+++ server/src/controllers/utils/deck.ts
@@ -0,0 +1,19 @@
+import { deckErrors } from '../../configs/errorCodes';
+import { ApiError } from '../../class';
+import Deck from '../../database/models/deck';
+import { HttpCode } from '../../type';
+
+/**
+ * Finds a deck by its ID.
+ * @param {number} id - The ID of the deck to be found.
+ * @returns {Promise<Deck>} - A promise that resolves with the found deck model object.
+ * @throws {ApiError} - If the deck is not found, it throws an error with a message
+ * indicating the missing deck with the specific ID.
+ */
+export async function findDeckById(id: number): Promise<Deck> {
+  const deck: Deck | null = await Deck.findByPk(id);
+  if (!deck) {
+    throw new ApiError(deckErrors.ERR_DECK_NOT_FOUND, HttpCode.NotFound);
+  }
+  return deck;
+}
