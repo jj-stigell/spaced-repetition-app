df116c6a91ecb766a20d52b715e3d61bf26d019f
Jere Stigell <jj.stigell@gmail.com>
2023-05-06 16:57:11 +0300

    update api list

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -6,3 +6,5 @@ export const resendConfirmation: string = '/api/v1/account/confirmation/resend' 
 export const resetPassword: string = '/api/v1/account/password/reset' // POST for link, PATCH for resetting
 export const setPassword: string = '/api/v1/account/password' // PATCH for setting a new password
 export const changeJlptLevel: string = '/api/v1/account/jlpt-level' // PATCH for updating
+export const getCategories: string = '/api/v1/category' // GET for fetching categories
+export const getDecks: string = '/api/v1/deck' // GET for fetching decks
