3cce4dcf576d6f00648f6093f68adf5c8ae03163
Jere Stigell <jj.stigell@gmail.com>
2023-05-13 21:59:51 +0300

    refacotre error page

--- client/src/pages/error/index.tsx
+++ client/src/pages/error/index.tsx
@@ -1,14 +1,11 @@
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import React from 'react'
 
 import { useTranslation } from 'react-i18next'
-import { useRouteError, isRouteErrorResponse, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { dashboard } from '../../config/path'
 
-function ErrorPage (): JSX.Element {
+function ErrorPage ({ children }: { children: JSX.Element }): JSX.Element {
   const { t } = useTranslation()
-  const error = useRouteError()
-  console.error(error)
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '50px' }}>
@@ -16,14 +13,7 @@ function ErrorPage (): JSX.Element {
       <p>{t('pages.error.description')}</p>
       <br/>
       <p>{t('pages.error.errorMessage')}</p>
-      <p>
-        {(isRouteErrorResponse(error))
-          ? <i>{error.statusText}</i>
-          : (error instanceof Error)
-              ? <i>{error.message}</i>
-              : <i>{t('pages.error.unknowError')}</i>
-        }
-      </p>
+      <p>{children}</p>
       <br/>
       <Link to={dashboard}>
         {t('pages.error.link')}
