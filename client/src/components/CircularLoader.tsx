8511b6d26809f96d698d9523fb34d21afb04a6d3
Jere Stigell <jj.stigell@gmail.com>
2023-05-13 23:43:58 +0300

    update loader

--- client/src/components/CircularLoader.tsx
+++ client/src/components/CircularLoader.tsx
@@ -5,7 +5,12 @@ import { Box, CircularProgress } from '@mui/material'
 
 function CircularLoader (): JSX.Element {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '50vh'
+    }}>
       <CircularProgress color='inherit' />
     </Box>
   )
