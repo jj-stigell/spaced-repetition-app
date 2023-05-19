55ff6c4cfbc4ab4a128a78bd832f423cfba7b38c
Jere Stigell <jj.stigell@gmail.com>
2023-05-19 18:03:19 +0300

    update study selector

--- client/src/pages/decks/StudySelector.tsx
+++ client/src/pages/decks/StudySelector.tsx
@@ -1,32 +1,35 @@
 import React from 'react'
 
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
+import LockIcon from '@mui/icons-material/Lock'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Modal from '@mui/material/Modal'
 import Typography from '@mui/material/Typography'
-import { useTranslation } from 'react-i18next'
-import { useNavigate } from 'react-router-dom'
 
 const style = {
   position: 'absolute' as 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 300,
+  width: 350,
   bgcolor: 'background.paper',
-  border: '1px solid #e70a28',
+  border: '3px solid #fad25f',
+  borderRadius: 3,
   boxShadow: 24,
   p: 4
 }
 
 const buttonStyle = {
-  bgcolor: '#bbbbbb',
+  bgcolor: '#f7e09e',
   '&:hover': {
-    bgcolor: '#e70a28'
+    bgcolor: '#fad25f'
   },
+  color: 'black',
   marginBottom: 3,
   flexGrow: 0,
-  width: '50%',
+  width: '75%',
   alignSelf: 'center'
 }
 
@@ -45,15 +48,16 @@ function StudyOptions (
     <Modal
       open={open}
       onClose={() => { setOpen(!open) }}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
+      aria-labelledby="modal-select-study-type"
+      aria-describedby="modal-select-study-due-or-all-cards"
     >
       <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ textAlign: 'center', marginBottom: 3 }}>
+        <Typography id="study-type-title" variant="h6" component="h2" sx={{ textAlign: 'center', marginBottom: 3 }}>
           {t('modals.studySelector.title')}
         </Typography>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <Button sx={buttonStyle} onClick={() => { handleStudyOptions(deckId, true) }}>
+          <Button sx={buttonStyle} disabled onClick={() => { handleStudyOptions(deckId, true) }}>
+            <LockIcon fontSize="small" />
             {t('modals.studySelector.studyDueAndNew')}
           </Button>
           <Button sx={buttonStyle} onClick={() => { handleStudyOptions(deckId, false) }}>
