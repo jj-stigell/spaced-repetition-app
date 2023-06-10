266f69911c177c1501b396037c145258fc1b3dd6
Jere Stigell <jj.stigell@gmail.com>
2023-06-10 22:38:31 +0300

    update sql files

---
+++ server/dbBaseData/card_translation_en.sql
@@ -0,0 +1,1 @@
+INSERT INTO card_translation(card_id,language_id,keyword,created_at,updated_at,options) VALUES (1,'EN','Mouth',NOW(),NOW(),'[{ "option": "car", "correct": true }, { "option": "airplane", "correct": false }, { "option": "boat", "correct": false }, { "option": "bicycle", "correct": false }]');
