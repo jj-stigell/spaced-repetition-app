bb2a7169f5f52088215bfd7c149f4e62af3f5b38
Jere Stigell <jj.stigell@gmail.com>
2023-12-25 15:22:41 +0200

    update

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { constants } from 'src/config/constants'
+import { constants } from '../../config/constants'
+import routes from '../../config/routes'
 
 export default function Logo (): React.JSX.Element {
   return (
-    <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">{constants.appName} App</Link>
+    <Link to={routes.dashboard} className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">{constants.appName}</Link>
   )
 }
